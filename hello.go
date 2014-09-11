028d3bc44f21b0dff0928f13c44d3fca430189de
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-10 23:58:14 -0400

    Added time library. Added simple time display.

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
 
 func main() {
-    fmt.Printf("Hello World!\n")
+    fmt.Println("Hello World!")
+    fmt.Println("The time is", time.Now())
 }
