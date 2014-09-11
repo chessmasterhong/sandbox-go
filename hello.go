d4fd734c852707a3bbd8644ec651672a2a9c2640
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-11 00:06:26 -0400

    Added simple random number generation.

--- hello.go
+++ hello.go
@@ -3,9 +3,11 @@ package main
 import (
     "fmt"
     "time"
+    "math/rand"
 )
 
 func main() {
     fmt.Println("Hello World!")
     fmt.Println("The time is", time.Now())
+    fmt.Println("I will roll two dices... their values are", rand.Intn(6), "and", rand.Intn(6))
 }
