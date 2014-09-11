2c603804334db8df46c08a5d01dbdd8f246b12e6
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-11 00:07:33 -0400

    Added simple time-based seed to random number generation.

--- hello.go
+++ hello.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
     fmt.Println("Hello World!")
+
     fmt.Println("The time is", time.Now())
+
+    rand.Seed(time.Now().Unix())
     fmt.Println("I will roll two dices... their values are", rand.Intn(6), "and", rand.Intn(6))
 }
