3ce25adb698904d7fcf7ff3e7328483e230e5b4e
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-11 00:26:40 -0400

    Added variables with initializers.

--- hello.go
+++ hello.go
@@ -6,12 +6,15 @@ import (
     "math/rand"
 )
 
+var t time.Time = time.Now()
+var s int64 = t.Unix()
+
 func main() {
     fmt.Println("Hello World!")
 
-    fmt.Println("The time is", time.Now())
+    fmt.Println("The time is", t)
 
-    rand.Seed(time.Now().Unix())
+    rand.Seed(s)
     a, b, c := sum(rand.Intn(5) + 1, rand.Intn(5) + 1)
     fmt.Println("I will roll two dice ... their values are", a, "and", b, "... their sum is", c)
 }
