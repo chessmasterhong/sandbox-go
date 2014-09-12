3c48b867400d1f4941d214b16fc0d6972558850a
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-11 21:12:30 -0400

    Added type conversions.

--- hello.go
+++ hello.go
@@ -3,24 +3,25 @@ package main
 import (
     "fmt"
     "time"
+    "math"
     "math/rand"
 )
 
-const PI float32 = 3.14
-
-var t time.Time = time.Now()
-var s int64 = t.Unix()
+const PI = math.Pi
+var pi float64 = PI
 
 func main() {
     fmt.Println("Hello World!")
 
+    var t time.Time = time.Now()
     fmt.Println("The time is", t)
 
-    rand.Seed(s)
+    rand.Seed(t.Unix())
     var a, b, c int = sum(rand.Intn(5) + 1, rand.Intn(5) + 1)
     fmt.Println("I will roll two dice ... their values are", a, "and", b, "... their sum is", c)
 
-    fmt.Println("Happy", PI, "Day!")
+    var pi float64 = float64(int(pi * 100)) / 100
+    fmt.Println("Happy", pi, "Day!")
 }
 
 func sum(x int, y int) (int, int, int) {
