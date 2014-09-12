1cbd9c6874c4203e5f5f23ff96800c8bb2cc3738
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-11 23:20:21 -0400

    Converted BeerBottles to function.

--- hello.go
+++ hello.go
@@ -3,36 +3,44 @@ package main
 import (
     "fmt"
     "math"
-    //"math/rand"
+    "math/rand"
     "strconv"
-    //"time"
+    "time"
 )
 
 const PI = math.Pi
 var pi float64 = PI
 
 func main() {
-//    fmt.Println("Hello World!")
-//
-//    var t time.Time = time.Now()
-//    fmt.Println("The time is", t)
-//
-//    rand.Seed(t.Unix())
-//    var a, b, c int = sum(rand.Intn(5) + 1, rand.Intn(5) + 1)
-//    fmt.Println("I will roll two dice ... their values are", a, "and", b, "... their sum is", c)
-//
-//    var pi float64 = float64(int(pi * 100)) / 100
-//    fmt.Println("Happy", pi, "Day!")
-//
-//    for i := 10; i > 0; i-- {
-//        fmt.Println(i, "bottles of beer on the wall,", i, "bottles of beer. Take one down pass it around,", i - 1, "bottles of beer on the wall.")
-//    }
-    
-    FizzBuzz(100)
+    fmt.Println("Hello World!")
+
+    var t time.Time = time.Now()
+    fmt.Println("The time is", t)
+
+    rand.Seed(t.Unix())
+    var a, b, c int = sum(rand.Intn(5) + 1, rand.Intn(5) + 1)
+    fmt.Println("I will roll two dice ... their values are", a, "and", b, "... their sum is", c)
+
+    var pi float64 = float64(int(pi * 100)) / 100
+    fmt.Println("Happy", pi, "Day!")
+
+    //BeerBottles(99)
+
+    //FizzBuzz(100)
 }
 
 func sum(x int, y int) (int, int, int) {
     return x, y, x + y
+}
+
+func BeerBottles(b int) {
+    if b == 0 {
+        return
+    }
+
+    fmt.Println(b, "bottles of beer on the wall,", b, "bottles of beer. Take one down, pass it around,", b - 1, "bottles of beer on the wall.")
+
+    BeerBottles(b - 1)
 }
 
 func FizzBuzz(n int) {
