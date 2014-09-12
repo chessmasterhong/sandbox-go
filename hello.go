47f6f2ab5d3375d8cbe4a968399d9b4a286a0c5f
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-11 23:14:18 -0400

    FizzBuzz!

--- hello.go
+++ hello.go
@@ -2,29 +2,43 @@ package main
 
 import (
     "fmt"
-    "time"
     "math"
-    "math/rand"
+    //"math/rand"
+    "strconv"
+    //"time"
 )
 
 const PI = math.Pi
 var pi float64 = PI
 
 func main() {
-    fmt.Println("Hello World!")
+//    fmt.Println("Hello World!")
+//
+//    var t time.Time = time.Now()
+//    fmt.Println("The time is", t)
+//
+//    rand.Seed(t.Unix())
+//    var a, b, c int = sum(rand.Intn(5) + 1, rand.Intn(5) + 1)
+//    fmt.Println("I will roll two dice ... their values are", a, "and", b, "... their sum is", c)
+//
+//    var pi float64 = float64(int(pi * 100)) / 100
+//    fmt.Println("Happy", pi, "Day!")
+//
+//    for i := 10; i > 0; i-- {
+//        fmt.Println(i, "bottles of beer on the wall,", i, "bottles of beer. Take one down pass it around,", i - 1, "bottles of beer on the wall.")
+//    }
+    
+    for i := 1; i <= 100; i++ {
+        fmt.Printf(strconv.Itoa(i) + " ")
 
-    var t time.Time = time.Now()
-    fmt.Println("The time is", t)
+        if i % 3 == 0 {
+            fmt.Printf("Fizz")
+        }
+        if i % 5 == 0 {
+            fmt.Printf("Buzz")
+        }
 
-    rand.Seed(t.Unix())
-    var a, b, c int = sum(rand.Intn(5) + 1, rand.Intn(5) + 1)
-    fmt.Println("I will roll two dice ... their values are", a, "and", b, "... their sum is", c)
-
-    var pi float64 = float64(int(pi * 100)) / 100
-    fmt.Println("Happy", pi, "Day!")
-
-    for i := 10; i > 0; i-- {
-        fmt.Println(i, "bottles of beer on the wall,", i, "bottles of beer. Take one down pass it around,", i - 1, "bottles of beer on the wall.")
+        fmt.Printf("\n")
     }
 }
 
